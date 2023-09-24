b0ef9daf0e9107950ffaabfe43125c4bf45b0bfd
NikitaUssyukin <nikus0108@gmail.com>
2023-09-24 22:19:28 +0600

    Finished HW2

---
+++ HW2/kbtuapp/src/App.js
@@ -0,0 +1,28 @@
+import logo from './kbtu_logo_1680_473.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div>
+          <br />
+          <br />
+          <br />
+          <br />Live <br />A <br />Happy  <br />Life <br />AND
+        </div>
+        <a
+          className="App-link"
+          href="https://www.youtube.com/watch?v=xvFZjo5PgG0"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Study in KBTU!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
